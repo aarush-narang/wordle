9a10284447433c4405719115e3a6d626f1a5c838
aarush-narang <aarushnarang@gmail.com>
2022-11-15 00:44:46 -0800

    test

--- src/app.py
+++ src/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(main_router.main_router, url_prefix='/')
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('DOMAIN'), port=os.getenv('PORT'), ssl_context='adhoc', debug=True)
+    app.run()
